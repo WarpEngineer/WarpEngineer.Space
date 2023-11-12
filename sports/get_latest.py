cea291d0dde708b59395834200e01c0c389c75d4
WarpEngineer <WarpEngineer@users.noreply.github.com>
2023-11-12 09:23:37 -0600

    Updated to use new NHL API

--- sports/get_latest.py
+++ sports/get_latest.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 ###vim: set encoding=utf-8 :
+from __future__ import print_function
 
 import os
 # set time zone to eastern
@@ -12,14 +13,14 @@ import time
 from datetime import datetime, timedelta
 from tz import UTC, LocalTimezone
 
-VERSION = "version 0.8.3"
+VERSION = "version 0.9.0"
 
 # Date format = YYYY-MM-DD
-URL_NHL = "https://statsapi.web.nhl.com/api/v1/schedule?startDate=%s&endDate=%s&hydrate=team(leaders(categories=[points,goals,assists],gameTypes=[R])),linescore,broadcasts(all),tickets,game(content(media(epg),highlights(scoreboard)),seriesSummary),radioBroadcasts,metadata,decisions,scoringplays,seriesSummary(series)&site=en_nhl&teamId=&gameType=&timecode="
+URL_NHL = "https://api-web.nhle.com/v1/score/%s"
 URL_MLB = "https://bdfed.stitch.mlbinfra.com/bdfed/transform-mlb-scoreboard?stitch_env=prod&sortTemplate=4&sportId=1&startDate=%s&endDate=%s&gameType=E,S,R,F,D,L,W,A&=,,,,,,,&language=en&leagueId=103,104"
 
 def error(msg=None, sport=""):
-	print "# Oops! Something went wrong"
+	print("# Oops! Something went wrong")
 	if msg:
 		err_file = os.path.join(os.getenv("HOME"), "%s_error.%s.log" % (sport,time.strftime("%Y_%m_%d_%H_%M")))
 		f = open(err_file, "w")
@@ -28,9 +29,9 @@ def error(msg=None, sport=""):
 	return 1
 
 def format_nhl_game(game):
-	away_team = game['teams']['away']['team']['abbreviation']
-	home_team = game['teams']['home']['team']['abbreviation']
-	game_type = '' if game['gameType'].lower() == 'r' else '(pre-season)' if game['gameType'].lower() == 'pr' else '(post-season)'
+	away_team = game['awayTeam']['abbrev']
+	home_team = game['homeTeam']['abbrev']
+	game_type = '' if game['gameType'] == 2 else '(pre-season)' if game['gameType'] == 1 else '(post-season)'
 	game_number = ''
 	series_status = ''
 	if game_type != '':
@@ -39,39 +40,39 @@ def format_nhl_game(game):
 			series_status = game['seriesSummary']['seriesStatusShort']
 		except:
 			pass
-	print
-	print "# %s at %s %s %s" % ( away_team, home_team, game_number, game_type )
+	print()
+	print("# %s at %s %s %s" % ( away_team, home_team, game_number, game_type ))
 	if game_type != '' and series_status != '':
-		print "### Series: %s" % ( series_status )
-	current_state  = game['status']['codedGameState']
-	if current_state in ["3","4"]: # in progess
+		print("### Series: %s" % ( series_status ))
+	current_state  = game['gameState'].lower()
+	if current_state in ["live","crit"]: # in progess
 		current_period = game['linescore']['currentPeriodOrdinal']
 		current_period_ord = game['linescore']['currentPeriodOrdinal']
 		current_remain = game['linescore']['currentPeriodTimeRemaining']
 		away_scrore = game['teams']['away']['score']
 		home_scrore = game['teams']['home']['score']
 		if current_remain.lower() == "end":
-			print "## END of %s Period" % current_period_ord
+			print("## END of %s Period" % current_period_ord)
 		else:
-			print "## Period: %s   Time Remaining: %s" % (current_period, current_remain)
-		print "### %s: %s" % ( away_team, away_scrore )
-		print "### %s: %s" % ( home_team, home_scrore )
-	elif current_state in ["9"]: # postponed
-		print '## Postponed'
-	elif current_state in ["5", "6", "7"]: # final
-		away_scrore = game['teams']['away']['score']
-		home_scrore = game['teams']['home']['score']
-		current_period = game['linescore']['currentPeriod']
+			print("## Period: %s   Time Remaining: %s" % (current_period, current_remain))
+		print("### %s: %s" % ( away_team, away_scrore ))
+		print("### %s: %s" % ( home_team, home_scrore ))
+	elif current_state == "postponed":
+		print('## Postponed')
+	elif current_state.lower() in ["final","off"]:
+		away_scrore = game['awayTeam']['score']
+		home_scrore = game['homeTeam']['score']
+		current_period = game['periodDescriptor']['number']
 		overtime = ""
 		if int(current_period) > 3:
-			overtime = game['linescore']['currentPeriodOrdinal']
-		if game['linescore']['hasShootout']:
-			overtime = "OT/SO"
-		print "### %s: %s" % ( away_team, away_scrore )
-		print "### %s: %s" % ( home_team, home_scrore )
-		print "### Final %s" % overtime
-	elif current_state in ["1", "2"]: # preview, scheduled
-		start_time = game['gameDate']
+			overtime = game['periodDescriptor']['periodType']
+		# if game['linescore']['hasShootout']:
+			# overtime = "OT/SO"
+		print("### %s: %s" % ( away_team, away_scrore ))
+		print("### %s: %s" % ( home_team, home_scrore ))
+		print("### Final %s" % overtime)
+	elif current_state == 'fut': # future
+		start_time = game['startTimeUTC']
 		try:
 			t = datetime.strptime(start_time,"%Y-%m-%dT%H:%M:%SZ")
 			t1 = datetime(t.year,t.month,t.day,t.hour,t.minute,t.second,tzinfo=UTC())
@@ -79,7 +80,7 @@ def format_nhl_game(game):
 		except:
 			# just use as is then
 			pass
-		print "## Starts at %s" % start_time
+		print("## Starts at %s" % start_time)
 
 def format_mlb_game(game):
 	away_team = game['teams']['away']['team']['abbreviation']
@@ -92,32 +93,32 @@ def format_mlb_game(game):
 	else:
 		game_type = '(post-season)'
 	game_number = "" if game['doubleHeader'].lower() == "n" else "game #" + str(game['gameNumber'])
-	print
-	print "# %s at %s %s %s" % ( away_team, home_team, game_number, game_type )
+	print()
+	print("# %s at %s %s %s" % ( away_team, home_team, game_number, game_type ))
 	current_state = game['status']['codedGameState']
 	if current_state.lower() == "i": # in progress
 		current_inning = game['linescore']['currentInning']
 		current_inning_state = game['linescore']['inningState']
 		away_scrore = game['teams']['away']['score']
 		home_scrore = game['teams']['home']['score']
-		print "## %s of Inning %s" % (current_inning_state, current_inning)
-		print "### %s: %s" % ( away_team, away_scrore )
-		print "### %s: %s" % ( home_team, home_scrore )
+		print("## %s of Inning %s" % (current_inning_state, current_inning))
+		print("### %s: %s" % ( away_team, away_scrore ))
+		print("### %s: %s" % ( home_team, home_scrore ))
 	elif current_state.lower() in ["f","o"]: # final, over
 		current_inning = game['linescore']['currentInning']
 		scheduled_innings = game['linescore']['scheduledInnings']
 		away_scrore = game['teams']['away']['score']
 		home_scrore = game['teams']['home']['score']
 		if int(current_inning) > int(scheduled_innings):
-			print "## Final Score/%s" % current_inning
+			print("## Final Score/%s" % current_inning)
 		else:
-			print "## Final Score"
-		print "### %s: %s" % ( away_team, away_scrore )
-		print "### %s: %s" % ( home_team, home_scrore )
+			print("## Final Score")
+		print("### %s: %s" % ( away_team, away_scrore ))
+		print("### %s: %s" % ( home_team, home_scrore ))
 	elif current_state.lower() == 'd': # delayed, postponed
-		print '## Postponed: %s' % game['status']['reason']
+		print('## Postponed: %s' % game['status']['reason'])
 	elif current_state.lower() == 'u': # suspended
-		print '## %s' % game['status']['reason']
+		print('## %s' % game['status']['reason'])
 	elif current_state.lower() in ['s','p']: # scheduled, preview
 		start_time = game['gameDate']
 		try:
@@ -128,7 +129,7 @@ def format_mlb_game(game):
 		except:
 			# just use as is then
 			pass
-		print "## Starts at %s" % start_time
+		print("## Starts at %s" % start_time)
 
 def main():
 	# pointers: White Left Pointing Backhand Index Emoji, White Right Pointing Backhand Index Emoji
@@ -159,17 +160,22 @@ def main():
 	try:
 		op = urllib2.build_opener()
 		op.addheaders = [('User-Agent', 'Mozilla/5.0')]
-		f = op.open(URL % ( query_date, query_date ))
+		if sport == 'nhl':
+			f = op.open(URL % ( query_date ))
+		else:
+			f = op.open(URL % ( query_date, query_date ))
 	except:
 		return error("Failed to fetch", sport)
 
 	data_coded = f.read()
 	data_decoded = json.loads(data_coded)
 	try:
-                if len(data_decoded['dates']):
-                    game_date = data_decoded['dates'][0]['date']
-                else:
-                    game_date = 'this day'
+		if 'dates' in data_decoded and len(data_decoded['dates']):
+			game_date = data_decoded['dates'][0]['date']
+		elif 'currentDate' in data_decoded:
+			game_date = data_decoded['currentDate']
+		else:
+			game_date = 'this day'
 	except:
 		return error(data_coded, sport)
 	try:
@@ -179,18 +185,20 @@ def main():
 		# just use it as is then
 		pass
 
-	print "# WarpEngineer's %s Scores" % sport.upper()
-	print "## Games for %s" % game_date
-	print day_links
-	print "### All times are Eastern"
-	print update_line
-	print "### Last update: %s" % time.asctime()
+	print("# WarpEngineer's %s Scores" % sport.upper())
+	print("## Games for %s" % game_date)
+	print(day_links)
+	print("### All times are Eastern")
+	print(update_line)
+	print("### Last update: %s" % time.asctime())
 
 	try:
-                if len(data_decoded['dates']):
-                    games = data_decoded['dates'][0]['games']
-                else:
-                    games = []
+		if 'dates' in data_decoded and len(data_decoded['dates']):
+			games = data_decoded['dates'][0]['games']
+		elif 'games' in data_decoded:
+			games = data_decoded['games']
+		else:
+			games = []
 	except:
 		return error(data_coded, sport)
 
@@ -201,9 +209,9 @@ def main():
 					format_nhl_game(game)
 				else:
 					format_mlb_game(game)
-				print
-		print
-		print VERSION
+				print()
+		print()
+		print(VERSION)
 
 	except:
 		return error(data_coded, sport)
